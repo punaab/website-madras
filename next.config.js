2c66c111109f8f04db79c6b6123b65c152b173a1
Punaab <notbitcoinceo@gmail.com>
2025-06-09 02:10:08 -0700

    Fix iframe sandboxing issues

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' https: data: 'unsafe-inline' 'unsafe-eval'; frame-ancestors 'self';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; frame-ancestors 'self';"
           }
         ]
       }
